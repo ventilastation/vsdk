59d2c838f9ec7ea64fcab9275553824e63a39bca
Alejandro J. Cura <alecu@protocultura.net>
2024-10-13 15:29:10 -0300

    Images precompiled to mpy for faster upload

--- apps/regenerate-images.sh
+++ apps/regenerate-images.sh
@@ -1,6 +1,7 @@
 cd ..
 . .venv/bin/activate
 cd tools
-python generate_images.py > ../apps/micropython/libs/imagenes.py
-python generate_strips.py  >> ../apps/micropython/libs/imagenes.py
-echo "Created apps/micropython/libs/imagenes.py"
+python generate_images.py > ../emulator/imagenes.py
+python generate_strips.py  >> ../emulator/imagenes.py
+mpy-cross ../emulator/imagenes.py -o ../apps/micropython/libs/imagenes.mpy
+echo "Updated emulator/imagenes.py and apps/micropython/libs/imagenes.mpy"
