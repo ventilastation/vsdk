77cf3b55251df07bf565ae44e843a99683bed208
Alejandro J. Cura <alecu@protocultura.net>
2025-05-11 11:00:38 -0300

    Emulator starts and stops micropython in a platform agnostic way

--- emulator/emu.py
+++ emulator/emu.py
@@ -1,3 +1,4 @@
+import subprocess, platform
 import comms
 import sys
 from itertools import cycle
@@ -11,7 +12,14 @@ led_count = 54
 
 display = False if "--no-display" in sys.argv else True
 
+UPY_ROOT = "../apps/micropython"
+UPY_EXEC = "z:/micropython.exe" if platform.system() == "Windows" else "micropython"
+
 try:
+    if display:
+        upy = subprocess.Popen([UPY_EXEC, "-X", "heapsize=8m", "main.py"], cwd=UPY_ROOT)  
     PygletEngine(led_count, comms.send, display)
 finally:
     comms.shutdown()
+    if display:
+        upy.kill()
