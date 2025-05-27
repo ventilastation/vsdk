08567aada87b13bb161ce9a95980d68859edeac5
Alejandro J. Cura <alecu@protocultura.net>
2025-05-27 19:59:44 -0300

    Restore ventap, and keep ventilatris separated

--- apps/images/ventilatris/stripedefs.py
+++ apps/images/ventilatris/stripedefs.py
@@ -2,8 +2,10 @@ stripes = [
     palettegroup(
         strip("target.png", frames=5),
         strip("bola.png", frames=12),
+        strip("ventilatris.png", frames=28)
     ),
     palettegroup(
         fullscreen("fondo.png"),
+        #fullscreen("lorem.png"),
     ),
 ]
