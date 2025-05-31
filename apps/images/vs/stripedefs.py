d403e4be8676eef03ae281c71af3ff2d05165854
rosu <mentitas@tuta.io>
2025-05-31 14:51:43 -0300

    Agregué las nuevas sprites!

--- apps/images/vs/stripedefs.py
+++ apps/images/vs/stripedefs.py
@@ -1,9 +1,16 @@
 stripes = [
     palettegroup(
-        strip("vga_pc734.png", frames=256),
-        strip("smeti.png",     frames=10),
-        strip("items.png",     frames=10),
-        strip("numerals.png",  frames=10),
-        strip("road.png",      frames=1),
+        strip("vga_pc734.png",   frames=256),
+        strip("smeti.png",       frames=10),
+        strip("items.png",       frames=10),
+        strip("numerals.png",    frames=10),
+        strip("road.png",        frames=1),
+        strip("lolero.png",      frames=4),
+        strip("fedora.png",      frames=8),
+        strip("burbujero.png",   frames=2),
+        strip("desodorante.png", frames=4),
+        strip("jabon.png",       frames=4),
+        strip("pala.png",        frames=8),
+        strip("pasto.png",       frames=2),
     )
 ]
