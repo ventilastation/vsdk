a1cfe23f645e6825d9d3157eee4b72cf7eb89126
Martín V <martinv@disroot.org>
2025-11-02 20:32:39 -0300

    Broken with AttributeError: 'NoneType' object has no attribute 'delete' in line 226

--- apps/micropython/apps/vissile.py
+++ apps/micropython/apps/vissile.py
@@ -48,7 +48,6 @@ class Misil:
         self.sprite.set_x(randrange(90,165))  # Tiene que estar dentro del área que puede cubrir la mira (x > 80 && x < 175)
         self.sprite.set_y(30)
         self.sprite.set_frame(0)
-        # self.sprite.disable = False
         
     def desactivar(self):
         self.sprite.disable()
@@ -57,55 +56,61 @@ class Misil:
         self.y_actual = self.sprite.y()
         self.sprite.set_y(self.y_actual + 1)
 
-# class Cascote:
-#     def __init__(self, torreta, target_center_x):
-#         self.sprite = Sprite()
-#         self.sprite.set_strip(stripes["cascote.png"])
-#         self.sprite.set_frame(0)
-#         self.sprite.set_perspective(1)
-#         self.target_center_x = target_center_x  # valor del **centro** del objetivo
-#         self.torreta = torreta
-#         self.delete = False
+class Cascote:
+    def __init__(self):
+        self.sprite = Sprite()
+        self.sprite.set_strip(stripes["cascote.png"])
+        self.sprite.set_frame(0)
+        self.sprite.set_perspective(1)
+        self.sprite.disable()
 
-#         # Torretas isquierdas, de izquierda a derecha
-#         if torreta == 1:
-#             self.sprite.set_x(64 - self.sprite.width())
-#             self.sprite.set_y(40)
-#         elif torreta == 2:
-#             self.sprite.set_x(64 - self.sprite.width())
-#             self.sprite.set_y(65)
-#         elif torreta == 3:
-#             self.sprite.set_x(64 - self.sprite.width())
-#             self.sprite.set_y(90)
-#         # Torretas derechas, de izquierda a derecha
-#         elif torreta == 4:
-#             self.sprite.set_x(192 - self.sprite.width())
-#             self.sprite.set_y(90 + 4)
-#             self.sprite.set_frame(1)
-#         elif torreta == 5:
-#             self.sprite.set_x(192 - self.sprite.width())
-#             self.sprite.set_y(65 + 4)
-#             self.sprite.set_frame(1)
-#         elif torreta == 6:
-#             self.sprite.set_x(192 - self.sprite.width())
-#             self.sprite.set_y(40 + 4)
-#             self.sprite.set_frame(1)
-    
-#     def mover(self):
+        self.delete = True
+
+    def activar(self, torreta, target_center_x):
+        self.torreta = torreta
+        self.target_center_x = target_center_x  # valor del **centro** del objetivo
+        self.sprite.set_frame(0)
+        self.delete = False
         
-#         self.x_actual = self.sprite.x()
-#         self.x_centro = self.x_actual + (self.sprite.width() // 2)
+        # Torretas isquierdas, de izquierda a derecha
+        if torreta == 1:
+            self.sprite.set_x(64 - self.sprite.width())
+            self.sprite.set_y(40)
+        elif torreta == 2:
+            self.sprite.set_x(64 - self.sprite.width())
+            self.sprite.set_y(65)
+        elif torreta == 3:
+            self.sprite.set_x(64 - self.sprite.width())
+            self.sprite.set_y(90)
+        # Torretas derechas, de izquierda a derecha
+        elif torreta == 4:
+            self.sprite.set_x(192 - self.sprite.width())
+            self.sprite.set_y(90 + 4)
+        elif torreta == 5:
+            self.sprite.set_x(192 - self.sprite.width())
+            self.sprite.set_y(65 + 4)
+        elif torreta == 6:
+            self.sprite.set_x(192 - self.sprite.width())
+            self.sprite.set_y(40 + 4)
+        
+    
+    def desactivar(self):
+        self.sprite.disable()
 
-#         if self.torreta == 1 or self.torreta == 2 or self.torreta == 3 :
-#             if self.x_centro + 2 < self.target_center_x:
-#                 self.sprite.set_x(self.x_actual + 1)
-#             else:
-#                 self.delete = True
-#         else:
-#             if self.x_centro + 2 > self.target_center_x:
-#                 self.sprite.set_x(self.x_actual - 1)
-#             else:
-#                 self.delete = True
+    def mover(self):    
+        x_actual = self.sprite.x()
+        centrer_x = x_actual + (self.sprite.width() // 2)
+
+        if self.torreta == 1 or self.torreta == 2 or self.torreta == 3 :
+            if centrer_x < self.target_center_x:
+                self.sprite.set_x(x_actual + 1)
+            else:
+                self.delete = True
+        else:
+            if centrer_x > self.target_center_x:
+                self.sprite.set_x(x_actual - 1)
+            else:
+                self.delete = True
             
             
 
@@ -134,14 +139,13 @@ class Misil:
 class Vissile(Scene):
     stripes_rom = "vissile"
 
-    
-
     def on_enter(self):
         super(Vissile, self).on_enter()
 
         self.mira = Mira()
         # self.explosiones = []
-        # self.cascotes = []
+        self.cascotes_reserva = [Cascote(), Cascote(), Cascote(), Cascote(), Cascote()]
+        self.cascotes_activos = []
         
         self.misiles_reserva = [Misil(), Misil(), Misil(), Misil()]
         self.misiles_activos = []
@@ -173,37 +177,41 @@ class Vissile(Scene):
         if director.was_pressed(director.BUTTON_A):
 
             # Temporalmente también usar el clic para crear misiles
-            if len(self.misiles_reserva)  > 0:
+            if len(self.misiles_reserva) > 0:
                 m = self.misiles_reserva.pop()  # Obtengo misil de reserva
                 m.activar()  # Lo reinicializo
                 self.misiles_activos.append(m)  # Lo agrego a los misiles activos
 
-            # target_x_center = self.mira.sprite.x() - (self.mira.sprite.width() // 2)
-            # if self.mira.sprite.x() < 128 - self.mira.sprite.width() // 2:
-            #     if self.mira.sprite.y() == 40:
-            #         self.cascotes.append(Cascote(1, target_x_center - self.mira.sprite.width()//2))
-            #     elif self.mira.sprite.y() == 65:
-            #         self.cascotes.append(Cascote(2, target_x_center - self.mira.sprite.width()//2))
-            #     elif self.mira.sprite.y() == 90:
-            #         self.cascotes.append(Cascote(3, target_x_center - self.mira.sprite.width()//2))
-            # else:
-            #     if self.mira.sprite.y() == 40:
-            #         self.cascotes.append(Cascote(6, target_x_center + self.mira.sprite.width()//2))
-            #     elif self.mira.sprite.y() == 65:
-            #         self.cascotes.append(Cascote(5, target_x_center + self.mira.sprite.width()//2))
-            #     elif self.mira.sprite.y() == 90:
-            #         self.cascotes.append(Cascote(4, target_x_center + self.mira.sprite.width()//2))
+            if len(self.cascotes_reserva) > 0:
+                c = self.cascotes_reserva.pop()
+                
+                mira_center_x = self.mira.sprite.x() + (self.mira.sprite.width() // 2)
+                if mira_center_x < 128 :
+                    if self.mira.sprite.y() == 40:
+                        self.cascotes_activos.append(c.activar(1, mira_center_x))
+                    elif self.mira.sprite.y() == 65:
+                        self.cascotes_activos.append(c.activar(2, mira_center_x))
+                    elif self.mira.sprite.y() == 90:
+                        self.cascotes_activos.append(c.activar(3, mira_center_x))
+                else:
+                    if self.mira.sprite.y() == 40:
+                        self.cascotes_activos.append(c.activar(6, mira_center_x))
+                    elif self.mira.sprite.y() == 65:
+                        self.cascotes_activos.append(c.activar(5, mira_center_x))
+                    elif self.mira.sprite.y() == 90:
+                        self.cascotes_activos.append(c.activar(4, mira_center_x))
             
 
         # Actualizar misiles
-        for m in self.misiles_activos:
-            if m.sprite.y() > 120:
-                m.desactivar()
-                self.misiles_activos.remove(m)
-                self.misiles_reserva.append(m)
-                # TODO Take damage!
-            else:
-                m.mover()
+        if len(self.misiles_activos) > 0:
+            for m in self.misiles_activos:
+                if m.sprite.y() > 120:
+                    m.desactivar()
+                    self.misiles_activos.remove(m)
+                    self.misiles_reserva.append(m)
+                    # TODO Take damage!
+                else:
+                    m.mover()
         
         # Actualizar explosiones
         # for e in self.explosiones:
@@ -212,17 +220,19 @@ class Vissile(Scene):
         #         e.sprite.disable()
         #         self.explosiones.remove(e)
         
-        # for c in self.cascotes:
-        #     if c.delete:
-        #         center_x = c.sprite.x() + (c.sprite.width() // 2)
-        #         center_y = c.sprite.y() - (c.sprite.height() // 2)
-        #         e = Explosion(center_x, center_y)
-        #         # TODO: Sólo permitir una cantidad determinada de explosiones al mismo tiempo para evitar flooding
-        #         c.sprite.disable()
-        #         self.cascotes.remove(c)
-        #         self.explosiones.append(e)
-        #     else:
-        #         c.mover()
+        # Actualizar cascotes
+        if len(self.cascotes_activos) > 0:
+            for c in self.cascotes_activos:
+                if c.delete:
+                    # center_x = c.sprite.x() + (c.sprite.width() // 2)
+                    # center_y = c.sprite.y() - (c.sprite.height() // 2)
+                    # e = Explosion(center_x, center_y)
+                    c.desactivar()
+                    self.cascotes_activos.remove(c)
+                    self.cascotes_reserva.append(c)
+                    # self.explosiones.append(e)
+                else:
+                    c.mover()
 
         # Salir
         if director.was_pressed(director.BUTTON_D):
