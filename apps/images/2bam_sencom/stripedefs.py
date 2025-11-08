50a29c043cde11afda7d08e1ed703480a3cc92de
nitram <8461554+2bam@users.noreply.github.com>
2025-11-08 01:27:24 +0100

    Update sencom (Nov 8)
    Add intro and lose screen
    Persist hiscore
    Add garbage collect per level
    Add garbage collect for emulator set_palette
    Add over-wave speed up for fingers
    Refactor palette modifications to PaletteHelper
    Tweak crosshair speed

--- apps/images/2bam_sencom/stripedefs.py
+++ apps/images/2bam_sencom/stripedefs.py
@@ -11,12 +11,21 @@ stripes = [
         # strip("texto_intro.png", frames=1), 
         strip("trail.png",frames=35),
         strip("target.png",frames=5),  
-        strip("warning.png",frames=1),  
+        strip("warning.png",frames=1),
+
         strip("dedo-frames65-neg.png",frames=13),
         strip("dedo-frames45-neg.png",frames=13),
         strip("dedo-frames0.png",frames=13),
         strip("dedo-frames45.png",frames=13),
         strip("dedo-frames65.png",frames=13),
+        
+        strip("font8_top.png", frames=63),
+        strip("font8_bottom.png", frames=63),
+        fullscreen("aspas.png"),
+    ),
+    palettegroup(
+        strip("2bam_sencom_top.png", frames=1),
+        strip("texto_intro_strip.png", frames=18),
 
         # strip("font_hiscore_8px_bottom.png",frames=12),  
         # strip("texto_intro.png", frames=1), 
